2abcbf14473fb36672e044fdbdbb1fab1660e4ab
Ville Talonpoika <ville@talonpoika.me>
2026-01-29 17:04:58 +0200

    Remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import eslintPluginAstro, * as eslintPluginSvelte from "eslint-plugin-astro";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
 	...eslintPluginAstro.configs.recommended,
 	...eslintPluginSvelte.configs["flat/recommended"],
-	eslintConfigPrettier,
 ];
